7690e54584ff332b5a03c0f091feabc873fcbdb8
caryoscelus <caryoscelus@gmx.com>
2016-08-19 18:12:37 +0300

    add hp to pc and misc

--- PCShip.gd
+++ PCShip.gd
@@ -2,9 +2,18 @@
 
 extends RigidBody2D
 
-var direction = Vector2(0, 0)
-export var speed = 200
+signal hurt
+
 const Bullet = preload("Bullet.tscn")
+
+onready var gunpoint = get_node("gunpoint")
+onready var shape = get_node("shape")
+
+export var WIDTH = 40
+export var speed = 200
+export var maxhp = 3
+var hp setget set_hp
+var direction = Vector2(0, 0)
 
 const ACTIONS = {
 	move_up = Vector2(0, -1),
@@ -14,6 +23,7 @@ const ACTIONS = {
 }
 
 func _ready():
+	set_hp(3)
 	set_process_input(true)
 
 func _input(event):
@@ -33,12 +43,37 @@ func _update_velocity():
 	set_linear_velocity(direction*speed)
 
 func _on_body_enter(body):
-	pass
+	if "owner" in body and body.owner == self:
+		return
+	if body == get_node("../borders"):
+		return
+	emit_signal("hurt")
+
+func _on_hurt():
+	self.hp -= 1
+	if hp <= 0:
+		queue_free()
+
+func set_hp(new_hp):
+	if hp == new_hp:
+		return
+	hp = new_hp
+	var sprite = get_node("sprite")
+	for s in get_tree().get_nodes_in_group("sprites"):
+		if s != sprite:
+			s.queue_free()
+	for i in range(hp-1):
+		sprite = sprite.duplicate()
+		sprite.set_offset(sprite.get_offset()+Vector2(WIDTH, 0))
+		add_child(sprite)
+	gunpoint.set_pos(Vector2(hp*WIDTH, 0))
+	shape.set_pos(Vector2((hp-1)*WIDTH/2.0, 0))
+	shape.set_scale(Vector2(hp*2.0/3+1.0/3, 1))
 
 func shoot_once():
 	var bullet = Bullet.instance()
 	bullet.owner = self
-	bullet.set_pos(get_pos())
+	bullet.set_pos(gunpoint.get_global_pos())
 	bullet.set_linear_velocity(Vector2(bullet.speed, 0))
 	get_node("..").add_child(bullet)
 
