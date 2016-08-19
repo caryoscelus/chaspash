598d538be63ced95875222e0c436f4a7f6c7a060
caryoscelus <caryoscelus@gmx.com>
2016-08-19 18:52:17 +0300

    proper enemy spawning

--- Enemy.gd
+++ Enemy.gd
@@ -3,9 +3,13 @@
 extends RigidBody2D
 
 signal left_space_area
+signal enter_space_area
 
 func _on_body_enter(body):
 	queue_free()
 
 func _on_left_space_area():
 	queue_free()
+
+func _on_enter_space_area():
+	set_mode(MODE_RIGID)
--- Space.gd
+++ Space.gd
@@ -10,5 +10,8 @@ func _ready():
 func _fixed_process(dt):
 	set_pos(get_pos()+Vector2(velocity*dt, 0))
 
+func _on_body_enter(body):
+	body.emit_signal("enter_space_area")
+
 func _on_body_exit(body):
 	body.emit_signal("left_space_area")
