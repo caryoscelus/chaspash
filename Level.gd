e9f9c08a6d8bc4b43b6a6dcf262ab569a5c10b78
caryoscelus <caryoscelus@gmx.com>
2016-08-20 14:17:17 +0300

    minor refactor

--- Level.gd
+++ Level.gd
@@ -6,7 +6,7 @@ export(String, FILE, "*.tscn") var next_level
 const GameOver = preload("res://GameOver.tscn")
 onready var hud = get_node("hud")
 onready var space = get_node("Space")
-onready var player = get_tree().get_root().get_node("PlayerInfo")
+onready var player = get_node("/root/PlayerInfo")
 
 func _ready():
 	player.connect("score_changed", self, "update_score")
@@ -16,11 +16,11 @@ func _on_end_enter(body):
 	body.emit_signal("end_of_level")
 
 func _on_change_level():
-	if next_level:
-		get_tree().change_scene(next_level)
-	else:
+	var scene = next_level
+	if not scene:
 		# duh, can we just read main_scene?
-		get_tree().change_scene("res://Menu.tscn")
+		scene = "res://Menu.tscn"
+	get_tree().change_scene(scene)
 
 func _on_game_over():
 	hud.add_child(GameOver.instance())
